9eb6d4357c59bcc8f400b369d67c59ab59f02ac4
Kevin Morgan <kevinm416@gmail.com>
2012-04-16 18:51:00 -0700

    Command line args added

--- ircclient.py
+++ ircclient.py
@@ -5,6 +5,8 @@ import sets
 import time
 from threading import Thread
 from Queue import Queue, Empty
+import traceback
+import itertools
 
 class IrcClient(Thread):
     def __init__(self, nick, host='localhost', port=6667):
@@ -13,44 +15,62 @@ class IrcClient(Thread):
         self.nick = nick
         self.host = host
         self.port = port
-        self.sock = socket.socket()
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.msgQ = Queue()
-        self.msgQ.put("NICK %s\r\n" % self.nick)
-        self.msgQ.put("USER %s %s bla :%s\r\n" % 
-            (self.nick, self.host, self.nick))
+        self.msgQ.put(("NICK %s\r\n" % self.nick, 0))
+        self.msgQ.put(("USER %s %s bla :%s\r\n" % 
+            (self.nick, self.host, self.nick), 0))
         
     def enter(self, channel):
         if not channel in self.channels:
-            self.msgQ.put("JOIN %s\r\n" % channel)
+            self.msgQ.put(("JOIN %s\r\n" % channel, 0))
             self.channels.add(channel)
         else:
             raise Exception("Trying to join a channel you are already in")
 
     def leave(self, channel):
         if channel in self.channels:
-            self.msgQ.put("PART %s\r\n" % channel)
+            self.msgQ.put(("PART %s\r\n" % channel, 0))
             self.channels.remove(channel)
         else:
             raise Exception("Trying to leave a channel you are not in")
 
     def quit(self):
-        self.msgQ.put("QUIT\n")
+        self.msgQ.put(("QUIT\n", 0))
     
     def send(self, channel, msg):
         if channel in self.channels:
             print 'put', msg, 'in queue'
-            self.msgQ.put("PRIVMSG %s %s\r\n" % (channel, msg))
+            self.msgQ.put(("PRIVMSG %s %s\r\n" % (channel, msg), 2))
         else:
             raise Exception("Trying to send to a channel you are not connected to")
-
+    
+    def socketSend(self, msg, wait=False):
+        sent = 0
+        (contents, wait) = msg
+        print 'contents:', contents, 'wait:', wait
+        msgLen = len(contents)
+        while sent < msgLen:
+            l = self.sock.send(contents)
+            sent += l
+            print 'sent:', contents[:l], 'sending:', contents[l:]
+            contents = contents[l:]
+        if wait:
+            time.sleep(wait)
+    
     def run(self):
         self.sock.connect((self.host, self.port))
         self.sock.setblocking(False)
         readbuffer=""
+        print 'entering loop'
         while 1:
-            print 'waiting...'
             try:
-                readbuffer=readbuffer+self.sock.recv(1024)
+                newMsg = self.sock.recv(1024)
+                print 'newMsg:', newMsg, 'delim', len(newMsg)
+                if len(newMsg) == 0:
+                    print 'about to return'
+                    return
+                readbuffer += newMsg
                 temp=string.split(readbuffer, "\n")
                 print 'temp:', temp
                 readbuffer=temp.pop( )
@@ -60,7 +80,6 @@ class IrcClient(Thread):
                     print 'line:', line
                     line=string.rstrip(line)
                     line=string.split(line)
-
                     if(line[0]=="PING"):
                         self.sock.sendall("PONG %s\r\n" % line[1])
             except:
@@ -69,16 +88,18 @@ class IrcClient(Thread):
             try:
                 while True:
                     msg = self.msgQ.get_nowait()
-                    print 'got msg:', msg
-                    self.sock.sendall(msg)
-                    print 'sent msg:', msg
+                    print 'msg:', msg
+                    self.socketSend(msg)
                     if msg == "QUIT\n":
-                        self.sock.shutdown(socket.SHUT_WR)
+                        print 'got quit', '!'*30
                         return
-            except Empty:
+            except:
                 pass
+                #exc_type, exc_value, exc_traceback = sys.exc_info()
+                #print "*** print_tb:"
+                #traceback.print_tb(exc_traceback, limit=1, file=sys.stdout)
 
-def enqueue_stream(stream, queue):
+def enqueue_stream(stream, queue, loop=False):
     for line in iter(stream.readline, ''):
         queue.put(line)
     stream.close()
@@ -86,13 +107,24 @@ def enqueue_stream(stream, queue):
 
 if __name__ == '__main__':
     print sys.argv
-    if not len(sys.argv) == 4:
-        print 'usage: ircclient <host> <channel> <nickname>'
-        sys.exit(1)
     
-    host = sys.argv[1]
-    channel = sys.argv[2]
-    nickname = sys.argv[3]
+    stream = sys.stdin    
+    host = 'localhost'
+    channel = ''
+    nickname = ''
+    loop = False
+
+    for item in sys.argv:
+        if item.startswith('-s='):
+            stream = open(item.split('=', 1)[1])
+        if item.startswith('-h='):
+            host = item.split('=', 1)[1]
+        if item.startswith('-c='):
+            channel = item.split('=', 1)[1]
+        if item.startswith('-n='):
+            nickname = item.split('=', 1)[1]
+        if item == '-l':
+            loop = True
 
     # start irc client thread
     c = IrcClient(nickname, host)
@@ -101,10 +133,10 @@ if __name__ == '__main__':
 
     # start enqueue thread
     q = Queue()
-    t = Thread(target=enqueue_stream, args=(sys.stdin, q))
+    t = Thread(target=enqueue_stream, args=(stream, q, loop))
     t.daemon = True
     t.start()
-
+    
     while True:
         try:
             line = q.get(True)
@@ -121,6 +153,7 @@ if __name__ == '__main__':
     
     c.join()
     print 'done'
+
 
 
 
